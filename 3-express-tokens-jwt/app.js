4e28484f78d846fd5a605aaaa7085621be80db63
losrobbos <losrobbos@web.de>
2022-08-25 23:08:55 +0200

    Completed JWT without cookies example

--- 3-express-tokens-jwt/app.js
+++ 3-express-tokens-jwt/app.js
@@ -1,10 +1,12 @@
 const dotenv = require("dotenv")
 const config = dotenv.config() // parse .env file and put variables into process.env
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 console.log(config)
 
 const express = require('express'); // ES6 module
 const animalRouter = require('./routes/animal.router');
+const User = require("./models/User.model");
 require("./db-connect")
 const app = express();
 
@@ -19,9 +21,45 @@ app.get('/', (req, res) => {
 });
 
 
-app.get("/login", (req, res) => {
-  const user = { _id: "r123", name: "Rob" }
-  const token = jwt.sign(user, JWT_SECRET)
+app.post("/signup", async (req, res) => {
+
+  try {
+    req.body.password = bcrypt.hashSync(req.body.password, 10)
+    const user = await User.create(req.body)
+    res.json(user)
+  }
+  catch(err) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+})
+
+app.post("/login", async (req, res) => {
+
+  const { email, password } = req.body
+
+  // GET user from "database" (fake) 
+  // const user = { _id: "r123", name: "Rob" }
+  const user = await User.findOne({ email })
+
+  if(!user) {
+    return res.status(400).json({
+      error: "Wrong login data"
+    })
+  }
+
+  if(!bcrypt.compareSync(password, user.password)) {
+    return res.status(400).json({
+      error: "Wrong login data"
+    });
+  }
+
+  // Create token from user data
+  const tokenData = { _id: user._id, email: user.email }
+  const token = jwt.sign(tokenData, JWT_SECRET)
+  
   res.send({
     token
   })
